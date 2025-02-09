1be8390b86ec7ea1d4ffbf1eb76068c36bc8e421
mavdotj <consorceo300@icloud.com>
2025-02-09 14:25:35 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,57 @@
-import { c, Wooter } from "jsr:@bronti/wooter";
-import { errorResponse, redirectResponse } from "jsr:@bronti/wooter/util";
+import { c, Wooter } from "@bronti/wooter";
+import { errorResponse, redirectResponse } from "@bronti/wooter/util";
 
 const wooter = new Wooter().useMethods();
 const kv = await Deno.openKv();
 
 const key = Deno.env.get("API_KEY");
 
-wooter.route(c.chemin(c.pMultiple(c.pString("pathParts"), false)))
+function nestObjects(objects: Deno.KvEntry<string>[]) {
+  let result = {};
+  
+  objects.forEach(({ key, ...obj }) => {
+    let current = result;
+    
+    key.forEach((k, index) => {
+      if (!current[k]) {
+        current[k] = {};
+      }
+      
+      if (index === key.length - 1) {
+        current[k] = obj.value;
+      }
+      
+      current = current[k];
+    });
+  });
+  
+  return result;
+}
+
+function traverseAndFormat(obj, depth = 0) {
+  let result = '';
+  
+  for (const key in obj) {
+    result += ' '.repeat(depth * 4) + '/' + key + '\n';
+    
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      result += traverseAndFormat(obj[key], depth + 1);
+    } else {
+      result += ' '.repeat((depth + 1) * 4) + obj[key] + '\n';
+    }
+  }
+  
+  return result;
+}
+
+wooter.GET(c.chemin('list'), async ({ resp }) => {
+  const result = kv.list<string>({ prefix: [] });
+  const map = nestObjects(await Array.fromAsync(result))
+
+  resp(new Response(traverseAndFormat(map)))
+})
+
+wooter.route(c.chemin(c.pMultiple(c.pString("pathParts"), true)))
   .GET(async ({ resp, params: { pathParts } }) => {
     const result = await kv.get<string>(pathParts);
     let destination: string;
